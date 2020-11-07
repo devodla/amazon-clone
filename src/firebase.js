a1da89fe2ba206f5ba41cc7d4d48f92c763cb248
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-11-07 01:37:46 -0300

    feat(main): add firts hello clever programmer and firebase file

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA-wspoNf1NQwOZQjtZ7kiGoew2Bm1IIUk",
+  authDomain: "challenge-24edc.firebaseapp.com",
+  databaseURL: "https://challenge-24edc.firebaseio.com",
+  projectId: "challenge-24edc",
+  storageBucket: "challenge-24edc.appspot.com",
+  messagingSenderId: "1059624835274",
+  appId: "1:1059624835274:web:6c8fd3e90d5c174d52fc33",
+  measurementId: "G-4T71THH3XH"
+};
